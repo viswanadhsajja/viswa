1cfe9a324b85d2f6331a9a69424e4a12dfa04e3c
viswanadhsajja <168949167+viswanadhsajja@users.noreply.github.com>
2024-05-06 09:40:42 +0530

    Add files via upload

--- week5/Filhan.m
+++ week5/Filhan.m
@@ -1,1 +1,17 @@
+clc;
+clear all;
+close all;
+fileId=fopen('sin_t.txt','r');
+A=fscanf(fileIc,'%f');
+fclose(fileId);
+f=10;
+x=sin(2*pi*f*A);
+plot(A,x);
 
+
+clc;
+close all;
+x=0:0.01:1;
+fileId=fopen('sin_t.txt','w');
+fprint(fileId,'%f',x);
+fclose(fileId);
