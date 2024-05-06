e4f4c9864eb4ea90de33efd3b86147d294b18ad5
viswanadhsajja <168949167+viswanadhsajja@users.noreply.github.com>
2024-05-06 09:34:53 +0530

    Add files via upload

---
+++ week3/Dict.m
@@ -0,0 +1,19 @@
+f=[440,880,1320]
+amp=[0.5,0.3,0.2]
+s=cell(length(f),2)
+s(:, 1)=num2cell(f)
+s(:, 2)=num2cell(amp)
+t=0:0.001:1
+figure;
+hold on;
+for i=1:length(s)
+  f=s{i, 1}
+  amp=s{i, 2}
+  wave=amp*sin(2*pi*f*t)
+  plot(t,wave,'displayName',['frequecy',num2str(f),'Hz'])
+endfor
+hold off;
+xlabel('time(s)')
+ylabel('amplitude')
+title('sine wave')
+legend
--- week3/Dict.py
+++ week3/Dict.py
@@ -1,1 +1,9 @@
-
+import numpy as np
+from matplotlib import pyplot as plt
+t=np.arange(0,1,0.01)
+sin_dict={'s1':[2,5],'s2':[5,10],'s3':[3,7],'s4':[10,12],'s5':[1,2]}
+k=input('enter sinosidelkey to generate')
+if sin_dict[k]:
+	x=sin_dict[k][0]*np.sin(2*np.pi*sin_dict[k][1]*t)
+	plt.plot(t,x)
+	plt.show()
